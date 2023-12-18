33ee90ec8163d63dbdb24c5dae889a1794c5fece
Vangogh <13979320112@sina.cn>
2023-12-19 07:48:30 +0800

    delete: 前端删除无用部分

--- frontend/src-core/type/ClassConstructor.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/**
- * # 类包装
- * @author Hamm
- */
-export type ClassConstructor<T> = {
-  new(...args: any[]): T;
-};
--- frontend/src/model/permission/PermissionSystemDictionary.ts
+++
@@ -1,7 +0,0 @@
-import { CoreColor } from '@core/enum/CoreColor';
-import { CoreDictionaryArray } from '@core/model/extend/CoreDictionaryArray';
-
-export const PermissionSystemDictionary = CoreDictionaryArray.create([
-    { key: false, label: '普通权限', color: CoreColor.NORMAL },
-    { key: true, label: '系统权限', color: CoreColor.DANGER },
-]);
